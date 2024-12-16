8c06f4fa6b3c2b95f18d0f413a72a94757760f98
Sandeep <sandeepreddyys2001@gmail.com>
2024-12-16 09:16:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const paragragh= document.createElement("p");
+paragragh.id=intro;
+paragragh.textcontent= "this is my new paragraph"
+document.body.appendChild(paragragh)
+console.log(paragragh.textcontent)
